e6561e843838c67d875d0509e57389980787e947
AUNNOP\User <aunnop_sirisit@cmu.ac.th>
2018-02-22 20:50:08 +0700

    [mockup] link to view detail of course (math)

--- my-app/src/app/course/course-routing.module.ts
+++ my-app/src/app/course/course-routing.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AddCourseComponent} from './add-course/add-course.component';
+import {ListCourseComponent} from "./list-course/list-course.component";
+import {ViewCourseComponent} from "./view-course/view-course.component";
 
 const courseRoute: Routes =
   [
     {path: 'add-course', component: AddCourseComponent},
+    {path: 'course-list', component: ListCourseComponent},
+    {path: 'view-course', component: ViewCourseComponent},
     {
       path: '',
       redirectTo: '/login',
