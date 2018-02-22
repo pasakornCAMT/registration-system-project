877ccb1e691af815e0c1a1889d48f8e269b7a3d9
Apiwat Hantrakool <apiwat_hantrakool@cmu.ac.th>
2018-02-22 17:10:57 +0700

    add student component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { StudentComponent } from './student/student.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MenuComponent
+    MenuComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule
