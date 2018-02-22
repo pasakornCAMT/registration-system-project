f847f1638b7d9991e6ebb4c3a73689a06081c0ca
AUNNOP\User <aunnop_sirisit@cmu.ac.th>
2018-02-22 17:13:36 +0700

    -routing(app + course) -make login page as defult -edit menu to make it available to link

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { AddCourseComponent } from './course/add-course/add-course.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CourseRoutingModule} from "./course/course-routing.module";
 
 
 @NgModule({
@@ -16,7 +16,7 @@ import { AddCourseComponent } from './course/add-course/add-course.component';
     AddCourseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,CourseRoutingModule,AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
