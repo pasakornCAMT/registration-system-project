0a0168c9ee0166f322bbdde581faebaffe4f94b7
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-02-22 19:07:33 +0700

    create course list and view course components

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
-import { StudentComponent } from './student/list/student.component';
+import { StudentComponent } from './student/view-student/student.component';
 import { AddCourseComponent } from './course/add-course/add-course.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {CourseRoutingModule} from "./course/course-routing.module";
-import { TeacherComponent } from './teacher/list/teacher.component';
+import { TeacherComponent } from './teacher/view-teacher/teacher.component';
 import { ViewCourseComponent } from './course/view-course/view-course.component';
+import { ListCourseComponent } from './course/list-course/list-course.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { ViewCourseComponent } from './course/view-course/view-course.component'
     StudentComponent,
     AddCourseComponent,
     TeacherComponent,
-    ViewCourseComponent
+    ViewCourseComponent,
+    ListCourseComponent
   ],
   imports: [
     BrowserModule,CourseRoutingModule,AppRoutingModule
