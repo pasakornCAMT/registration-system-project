332b3aa8743d950000d0aa9806d5449df26bfd37
Pasakorn Kunchai <pasakorn_kunchai@cmu.ac.th>
2018-04-30 00:49:51 +0700

    prepare for view-course

--- my-app/src/app/course/view-course/view-course.component.spec.ts
+++ my-app/src/app/course/view-course/view-course.component.spec.ts
@@ -1,10 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewCourseComponent } from './view-course.component';
+import {DataService} from '../../service/data.service';
+import {Course} from '../course';
+import {Student} from '../../student/student';
+import {COURSES, TEACHERS} from '../../mocks';
+import {Teacher} from '../../teacher/teacher';
 
 describe('ViewCourseComponent', () => {
   let component: ViewCourseComponent;
   let fixture: ComponentFixture<ViewCourseComponent>;
-
 
 });
