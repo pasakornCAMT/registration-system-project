acc0d13ee9a9ec0ffc10f8dc5021af4495142aa1
Apiwat  Hantrakool <apiwat_hantrakool@cmu.ac.th>
2018-04-30 15:32:10 +0700

    Unit test : Student component (Complete)

--- my-app/src/app/student/view-student/student.component.spec.ts
+++ my-app/src/app/student/view-student/student.component.spec.ts
@@ -31,8 +31,8 @@ describe('StudentComponent', () => {
     service= TestBed.get(DataService);
   });
 
-  //findStudentByEmail()---------------------------------------------------------------------------------------
-  it('findStudentByEmail(): should return first student object by student email', () => {
+  //findStudentByEmail(email:string)---------------------------------------------------------------------------------------
+  it('findStudentByEmail(email:string): should return first student object by student email', () => {
     //Arrange
     studentEmail = 'student1@cmuSTU.ac.th';
     //Act
@@ -41,7 +41,7 @@ describe('StudentComponent', () => {
     expect(student).toEqual(STUDENTS[0]);
   });
 
-  it('findStudentByEmail(): should return second student object by student email', () => {
+  it('findStudentByEmail(email:string): should return second student object by student email', () => {
     //Arrange
     studentEmail = 'student2@cmuSTU.ac.th';
     //Act
@@ -50,7 +50,7 @@ describe('StudentComponent', () => {
     expect(student).toEqual(STUDENTS[1]);
   });
 
-  it('findStudentByEmail(): should return third student object by student email', () => {
+  it('findStudentByEmail(email:string): should return third student object by student email', () => {
     //Arrange
     studentEmail = 'student3@cmuSTU.ac.th';
     //Act
@@ -93,8 +93,8 @@ describe('StudentComponent', () => {
     expect(component.student.enrolled_course).toEqual(STUDENTS[2].enrolled_course);
   });
 
-  //showDetail()---------------------------------------------------------------------------------------------------------
-  it('showDetail(): should show details of first course by course ID',()=>{
+  //showDetail(id:string)---------------------------------------------------------------------------------------------------------
+  it('showDetail(id:string): should show details of first course by course ID',()=>{
     //Arrange
     courseID='1';
     //Act
@@ -103,7 +103,7 @@ describe('StudentComponent', () => {
     expect(service.courseDetail).toEqual(courseID);
   });
 
-  it('showDetail(): should show details of second course by course ID',()=>{
+  it('showDetail(id:string): should show details of second course by course ID',()=>{
     //Arrange
     courseID='2';
     //Act
@@ -112,7 +112,7 @@ describe('StudentComponent', () => {
     expect(service.courseDetail).toEqual(courseID);
   });
 
-  it('showDetail(): should show details of third course by course ID',()=>{
+  it('showDetail(id:string): should show details of third course by course ID',()=>{
     //Arrange
     courseID='3';
     //Act
@@ -121,8 +121,8 @@ describe('StudentComponent', () => {
     expect(service.courseDetail).toEqual(courseID);
   });
 
-  //removeCourse()----------------------------------------------------------------------------------------------------------
-  it('removeCourse(): should remove first course from the enrolled-course list by course id',()=>{
+  //removeCourse(index:number)----------------------------------------------------------------------------------------------------------
+  it('removeCourse(index:number): should remove first course from the enrolled-course list by course id',()=>{
     //Arrange
     courseIndex=0;
     //Act
@@ -131,7 +131,7 @@ describe('StudentComponent', () => {
     expect(component.courses.length).toBeLessThan(3);
   });
 
-  it('removeCourse(): should remove second course from the enrolled-course list by course id',()=>{
+  it('removeCourse(index:number): should remove second course from the enrolled-course list by course id',()=>{
     //Arrange
     courseIndex=1;
     //Act
@@ -140,7 +140,7 @@ describe('StudentComponent', () => {
     expect(component.courses.length).toBeLessThan(3);
   });
 
-  it('removeCourse(): should remove third course from the enrolled-course list by course id',()=>{
+  it('removeCourse(index:number): should remove third course from the enrolled-course list by course id',()=>{
     //Arrange
     courseIndex=2;
     //Act
@@ -148,5 +148,87 @@ describe('StudentComponent', () => {
     //Assert
     expect(component.courses.length).toBeLessThan(3);
   });
+
+  //calculateCredit()-----------------------------------------------------------------------------------------------------
+  it('calculateCredit(): should show the total credits and free of first student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student1@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.calculateCredit();
+    //Assert
+    expect(component.totalCredit).toEqual(12);
+    expect(component.totalFee).toEqual(240);
+  });
+
+  it('calculateCredit(): should show the total credits and free of second student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student2@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.calculateCredit();
+    //Assert
+    expect(component.totalCredit).toEqual(6);
+    expect(component.totalFee).toEqual(120);
+  });
+
+  it('calculateCredit(): should show the total credits and free of third student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student3@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.calculateCredit();
+    //Assert
+    expect(component.totalCredit).toEqual(3);
+    expect(component.totalFee).toEqual(60);
+  });
+
+  //viewGPA()---------------------------------------------------------------------------------------------------------------
+  it('viewGPA(): should show the GPA of first student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student1@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.viewGPA();
+    //Assert
+    expect(component.student.enrolled_course).toContain(component.courses[0].courseId);
+    expect(component.student.enrolled_course).toContain(component.courses[1].courseId);
+    expect(component.student.enrolled_course).toContain(component.courses[2].courseId);
+  });
+
+  it('viewGPA(): should show the GPA of second student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student2@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.viewGPA();
+    //Assert
+    expect(component.student.enrolled_course).toContain(component.courses[0].courseId);
+    expect(component.student.enrolled_course).toContain(component.courses[1].courseId);
+  });
+
+  it('viewGPA(): should show the GPA of third student by student email.',()=>{
+    //Arrange
+    studentEmail = 'student3@cmuSTU.ac.th';
+    component.student=component.findStudentByEmail(studentEmail);
+    component.student.enrolled_course;
+    component.generateCourseData();
+    //Act
+    component.viewGPA();
+    //Assert
+    expect(component.student.enrolled_course).toContain(component.courses[0].courseId);
+  });
+
+
 
 });
